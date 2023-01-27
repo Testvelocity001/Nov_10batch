2df140beb170269224c5ab976043830dff48f529
Testvelocity001 <123718812+Testvelocity001@users.noreply.github.com>
2023-01-27 20:26:29 +0530

    Add files via upload

---
+++ java/testclasses/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author punit
+ *
+ */
+package testclasses;
---
+++ src/main/java/pomClasses/InsuranceLoginPage.java
@@ -0,0 +1,13 @@
+package pomClasses;
+
+public class KiteLoginPage {
+	
+	
+	@findBy (xpath ="//a") private WebElement fName
+	@findBy (xpath ="//a") private WebElement lName
+	@findBy (xpath ="//a") private WebElement sName
+	@findBy (xpath ="//a") private WebElement mob	
+	@findBy (xpath ="//a") private WebElement email
+	@findBy (xpath ="//a") private WebElement dob	
+
+}
